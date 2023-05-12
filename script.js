d49d3f8d4358d522531227d862176251ece724dc
Mark Huber <markhuber0907@outlook.com>
2023-05-11 21:09:37 -0700

    Finish game by adding playRound and game functions

--- script.js
+++ script.js
@@ -6,31 +6,48 @@ function getComputerChoice() {
 }
 
 // function that plays a single round and declares winner
-function playRound(playerSelection, computerSelection) {
-    'ask for player choice'
-    'decide what each side selected, and who won'
-    return "string that declares winner like: 'You lose! paper beats rock'"
+function playRound() {
+    let playerSelection = getPlayerChoice();
+    let computerSelection = getComputerChoice();
+    if(playerSelection === computerSelection) return `A tie! You both picked ${playerSelection}`
+    if(playerSelection === "rock" && computerSelection === "scissors" ||
+       playerSelection === "paper" && computerSelection === "rock" ||
+       playerSelection === "scissors" && computerSelection === "paper") {
+        return `You win! ${playerSelection} beats ${computerSelection}`
+    } else {
+        return `You lose! ${computerSelection} beats ${playerSelection}`
+    }
 }
 
 // function that checks conditions of player choice
-function playerCheck(choice) {
+function getPlayerChoice(choice = "") {
     let newChoice = choice.toLowerCase();
     if(newChoice === "rock" || newChoice === "paper" || newChoice === "scissors") {
         return newChoice
     } else {
         newChoice = prompt("What is your choice?")
-        return playerCheck(newChoice)
+        return getPlayerChoice(newChoice)
     }
 }
 
 // function that plays 5 rounds, keeps score, and declares winner
 function game() {
-    'make a win and loss variable = 0'
-    'declare a loop that iterates 5 times'
-        'call playRound()'
-        'increment win or loss depending on result'
-    'decide if win or loss is greater'
-    return 'winner and score'
+    let wins = 0;
+    let losses = 0;
+    for(x = 0; x < 5; x++) {
+        result = playRound();
+        console.log(result)
+        if(result.charAt(4) === "w") wins++
+        else if(result.charAt(4) === "l") losses++
+        else continue;
+    }
+    if(wins === losses) {
+        return `The score was ${wins} to ${losses}. It was a tie game!`
+    } else if(wins > losses) {
+        return `The score was ${wins} to ${losses}. You won the game!`
+    } else {
+        return `The score was ${wins} to ${losses}. You lost the game.`
+    }
 }
 
-console.log(playerCheck("rack"));
+console.log(game());
