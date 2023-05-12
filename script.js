5a6dadd72c5380ed27b816213185e999e8929d23
Mark Huber <markhuber0907@outlook.com>
2023-05-12 12:58:25 -0700

    Remove game(), add buttons with event listeners
    To start to change our game to being played in a ui instead of in the
    console, added physical buttons with event listeners to get player
    selection, removed game function as it will no longer be needed.

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+// function that checks conditions of player choice
+function getPlayerChoice(choice = "") {
+    let newChoice = choice.toLowerCase();
+    if(newChoice === "rock" || newChoice === "paper" || newChoice === "scissors") {
+        return newChoice
+    } else {
+        newChoice = prompt("What is your choice?")
+        return getPlayerChoice(newChoice)
+    }
+}
+
 // function to create a randomly generated computer choice
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
@@ -19,35 +30,6 @@ function playRound() {
     }
 }
 
-// function that checks conditions of player choice
-function getPlayerChoice(choice = "") {
-    let newChoice = choice.toLowerCase();
-    if(newChoice === "rock" || newChoice === "paper" || newChoice === "scissors") {
-        return newChoice
-    } else {
-        newChoice = prompt("What is your choice?")
-        return getPlayerChoice(newChoice)
-    }
-}
-
-// function that plays 5 rounds, keeps score, and declares winner
-function game() {
-    let wins = 0;
-    let losses = 0;
-    for(x = 0; x < 5; x++) {
-        result = playRound();
-        console.log(result)
-        if(result.charAt(4) === "w") wins++
-        else if(result.charAt(4) === "l") losses++
-        else continue;
-    }
-    if(wins === losses) {
-        return `The score was ${wins} to ${losses}. It was a tie game!`
-    } else if(wins > losses) {
-        return `The score was ${wins} to ${losses}. You won the game!`
-    } else {
-        return `The score was ${wins} to ${losses}. You lost the game.`
-    }
-}
-
-console.log(game());
+const buttons = document.querySelectorAll(".buttons-display button");
+buttons.forEach(button => button.addEventListener("click", () => getPlayerChoice(button.className)))
+console.log(buttons)
